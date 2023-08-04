49067aad20cf33a8881bb0720462d328377ad585
ryardley <contact@rudiyardley.com>
2023-08-04 16:30:40 +1000

    Work in progress. Ethereum Provider

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Ethereum Provider Example
 
+Curl example 
+
+```bash
+curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' 127.0.0.1:8545
+```
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,32 @@
-test("twerks like miley", () => {
-  expect(true).toBe(true);
+import { providers } from "ethers";
+import { Provider } from "./index.js";
+
+test("Provider", async () => {
+  const ethereum = new Provider();
+  const provider = new providers.Web3Provider(ethereum);
+
+  const bal = await provider.getBalance(
+    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  );
+
+  expect(`${bal}`).toBe("10000000000000000000000");
+});
+
+test("Cannot send ETH", async () => {
+  const ethereum = new Provider();
+  const provider = new providers.Web3Provider(ethereum);
+  const signer = provider.getSigner(
+    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  );
+
+  try {
+    await signer.sendTransaction({
+      from: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+      to: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+      value: "1000000000000000000",
+      nonce: 1,
+    });
+  } catch (err) {
+    expect(`${err}`).toMatch("Unauthorized");
+  }
 });
